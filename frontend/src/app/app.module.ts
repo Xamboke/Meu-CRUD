b8fb38f0ec37bc3cb8dc90f60f0845152840540a
grey014 <josemsousan@gmail.com>
2021-04-29 14:14:01 -0300

    29/04/2021 Last update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http'
 
 // Material Imports------------------------------------------
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -41,7 +42,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
